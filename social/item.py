f7461402990df08743f33e9b7b22e841c55649da
Prasad Sunkari <prasad@synovel.com>
2011-03-07 11:27:25 +0530

    Item liking and unliking code moved to item.py

--- social/item.py
+++ social/item.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 from telephus.cassandra import ttypes
 from twisted.internet   import defer
@@ -6,12 +7,9 @@ from twisted.web        import server
 from twisted.python     import log
 from twisted.plugin     import getPlugins
 
-
-from social             import base, Db, utils, plugins
+from social             import base, Db, utils, feed, plugins
 from social.auth        import IAuthInfo
-from social             import utils
 from social.template    import render, renderScriptBlock
-from social             import feed
 
 
 class ItemResource(base.BaseResource):
@@ -71,7 +69,6 @@ class ItemResource(base.BaseResource):
             owner = utils.supercolumnsToDict([owner])
             args.update({"users":{convOwner:owner}})
             args.update({"items":{convId: conv}})
-
 
         renderers = []
 
@@ -173,34 +170,182 @@ class ItemResource(base.BaseResource):
         yield self.renderItem(request, False)
 
 
+    @defer.inlineCallbacks
+    def _like(self, request):
+        itemId = utils.getRequestArg(request, "id")
+        myId = request.getSession(IAuthInfo).username
+
+        # Check if I already liked the item
+        try:
+            cols = yield Db.get(itemId, "itemLikes", myId)
+            raise errors.InvalidRequest()
+        except ttypes.NotFoundException:
+            pass
+
+        # Get the item and the conversation
+        item = yield Db.get(itemId, "items", super_column="meta")
+        item = utils.supercolumnsToDict([item])
+
+        convId = item["meta"].get("parent", None)
+        conv = None
+        if convId:
+            conv = yield Db.get(convId, "items", super_column="meta")
+            conv = utils.supercolumnsToDict([conv])
+        else:
+            convId = itemId
+            conv = item
+
+        convOwnerId = conv["meta"]["owner"]
+        convType = conv["meta"]["type"]
+        convACL = conv["meta"]["acl"]
+
+        # TODO: Check if I have access to that item before liking it!
+
+        # Update the likes count
+        likesCount = int(item.get("likesCount", "0")) + 1
+        if likesCount % 5 == 0:
+            likesCount = yield Db.get_count(itemId, "itemLikes")
+
+        yield Db.insert(itemId, "items", str(likesCount), "likesCount", "meta")
+
+        timeuuid = uuid.uuid1().bytes
+        responseType = "L"
+        # 1. add user to Likes list
+        yield Db.insert(itemId, "itemLikes", timeuuid, myId)
+
+        # 2. add users to the followers list of parent item
+        yield Db.insert(convId, "items", "", myId, "followers")
+
+        # 3. update user's feed, feedItems, feed_*
+        yield feed.pushToFeed(myId, timeuuid, itemId, convId,
+                              responseType, convType, convOwnerId, myId)
+
+        # 4. update feed, feedItems, feed_* of user's followers/friends
+        yield feed.pushToOthersFeed(myId, timeuuid, itemId, convId, convACL,
+                                    responseType, convType, convOwnerId)
+
+        # 5. Broadcast to followers of the items
+        # TODO
+
+        # Finally, update the UI
+        # TODO
+
+    @defer.inlineCallbacks
+    def _unlike(self, request):
+        itemId = utils.getRequestArg(request, "id")
+        myId = request.getSession(IAuthInfo).username
+
+        # Make sure that I liked this item
+        try:
+            cols = yield Db.get(itemId, "itemLikes", myId)
+        except ttypes.NotFoundException:
+            raise errors.InvalidRequest()
+
+        # Get the item and the conversation
+        item = yield Db.get(itemId, "items", super_column="meta")
+        item = utils.supercolumnsToDict([item])
+
+        convId = item["meta"].get("parent", None)
+        conv = None
+        if convId:
+            conv = yield Db.get(convId, "items", super_column="meta")
+            conv = utils.supercolumnsToDict([conv])
+        else:
+            conv = item
+
+        convOwnerId = conv["meta"]["owner"]
+        convType = conv["meta"]["type"]
+        convACL = conv["meta"]["acl"]
+
+        # 1. remove the user from likes list.
+        yield Db.remove(itemId, "itemLikes", myId)
+
+        # Update the likes count
+        likesCount = int(item.get("likesCount", "1")) - 1
+        if likesCount % 5 == 0:
+            likesCount = yield Db.get_count(itemId, "itemLikes")
+        yield Db.insert(itemId, "items", str(likesCount), "likesCount", "meta")
+
+        responseType = 'L'
+        # 2. Don't remove the user from followers list
+        #    (use can also become follower by responding to item,
+        #        so user can't be removed from followers list)
+
+        # 3. delete from user's feed, feedItems, feed_*
+        yield feed.deleteFromFeed(myId, itemId, convId,
+                                  convType, myId, responseType)
+
+        # 4. delete from feed, feedItems, feed_* of user's friends/followers
+        yield feed.deleteFromOthersFeed(myId, itemId, convId, convType,
+                                        convACL, convOwnerId, responseType)
+
+        # Finally, update the UI
+        # TODO
+
+    @defer.inlineCallbacks
+    def _comment(self, request):
+        pass
+
+    @defer.inlineCallbacks
+    def _likes(self, request):
+        start = utils.getRequestArg();
+        pass
+
+    @defer.inlineCallbacks
+    def _responses(self, request):
+        pass
+
+
     def render_GET(self, request):
         segmentCount = len(request.postpath)
+        d = None
         if segmentCount == 0:
             d =  self.renderItem(request)
+        elif segmentCount == 1:
+            path = request.postpath[0]
+            if path == "responses":
+                self._responses(request)
+            if path == "likes":
+                self._likes(request)
+            elif path == 'like' :
+                d = self._like(request)
+            elif path == 'unlike':
+                d = self._unlike(request)
+            elif path == 'comment':
+                d = self._comment(request)
+
+        if d:
             def callback(res):
                 request.finish()
             def errback(err):
-                log.msg(err)
+                log.err(err)
                 request.setResponseCode(500)
                 request.finish()
             d.addCallbacks(callback, errback)
             return server.NOT_DONE_YET
+        else:
+            pass    # XXX: Throw error
 
     def render_POST(self, request):
-
-        def callback(res):
-            request.finish()
-        def errback(err):
-            log.msg(err)
-            request.setResponseCode(500)
-            request.finish()
-
         segmentCount = len(request.postpath)
-        if segmentCount == 1 and request.postpath[0] == 'new':
-            d =  self.createItem(request)
+        d = None
+
+        if segmentCount == 1:
+            path = request.postpath[0]
+            if path == 'new':
+                d =  self.createItem(request)
+            elif path == 'act':
+                d =  self.actOnItem(request)
+
+        if d:
+            def callback(res):
+                request.finish()
+            def errback(err):
+                log.err(err)
+                request.setResponseCode(500)
+                request.finish()
+
             d.addCallbacks(callback, errback)
             return server.NOT_DONE_YET
-        if segmentCount == 1 and request.postpath[0] == 'act':
-            d =  self.actOnItem(request)
-            d.addCallbacks(callback, errback)
-            return server.NOT_DONE_YET
+        else:
+            pass # XXX: 404 error
