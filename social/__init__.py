11c5a43cd0c4e55d66700a7c3cff5456065a552c
Prasad Sunkari <prasad@synovel.com>
2011-03-08 13:41:47 +0530

    Silly change to he way plugins are enumerated

--- social/__init__.py
+++ social/__init__.py
@@ -7,14 +7,17 @@ from ordereddict        import OrderedDict
 from telephus.protocol  import ManagedCassandraClientFactory
 from telephus.client    import CassandraClient
 from twisted.internet   import reactor
-from twisted.plugin     import getPlugins
+from twisted.plugin import getPlugins
 
 from social.isocial     import IItem
 
+
+# Read configuration
 Config = ConfigParser.ConfigParser()
 Config.read([os.path.abspath('etc/social.cfg')])
 
 
+# Connect to Cassandra
 _dbhost = Config.get("Cassandra", "Host")
 _dbport = Config.get("Cassandra", "Port")
 _keyspace = Config.get("Cassandra", "Keyspace")
@@ -22,20 +25,17 @@ _factory = ManagedCassandraClientFactory(_keyspace)
 reactor.connectTCP(_dbhost, int(_dbport), _factory)
 Db = CassandraClient(_factory)
 
+
+# Aliases for i18n
 _ = gettext.gettext
 __ = gettext.ngettext
 
-_positionMap = {}
-_positions = []
-for plugin in getPlugins(IItem):
-    _positions.append(plugin.position)
-    _positionMap[plugin.position] = plugin
 
-_positions.sort()
-plugins = OrderedDict()
-for position in _positions:
-    plugin = _positionMap[position]
-    plugins[plugin.itemType] = plugin
-
+# Map of all item type plugins
+_pluginList = sorted([x for x in getPlugins(IItem)], key=lambda x: x.position)
+plugins = {}
+for plg in _pluginList:
+    plugins[plg.itemType] = plg
+    
 
 __all__ = [Config, Db, _, __, plugins]
