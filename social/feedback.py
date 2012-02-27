09eefb57476aa05782a970db1374a6033e2882a1
Praveen Pothana <praveen@synovel.com>
2012-02-27 12:17:06 +0530

    feedback.py: entity related changes. r=prasad cr=297

--- social/feedback.py
+++ social/feedback.py
@@ -1,33 +1,40 @@
+import json
 from twisted.internet   import defer
-from twisted.web        import server
 
-from social             import db, utils, base, tags, _, config, errors, feed
+from social             import db, utils, base, tags, _, config, errors
 from social             import template as t
 from social.isocial     import IAuthInfo
-from social.logging     import log
 
 
 class FeedbackResource(base.BaseResource):
-    isLeaf=True
+    isLeaf = True
     _templates = ['item.mako']
 
-
     def renderFeedbackForm(self, request):
+        """render feedback dialog."""
         t.renderScriptBlock(request, "item.mako", "feedbackDialog",
                             False, "#feedback-dlg", "set")
         return True
 
-
     @defer.inlineCallbacks
     def postFeedback(self, request):
+        """creates a feedback item with feedback, feedback-{mood} tags.
+        Push item to feeback, feedback-{mood} tag followers.
+        Note: Item is owned by feedback-domain/synovel.com, not user sending
+        the feedback. Only users of feedback-domain/synovel.com can access
+        the item.
+        """
         comment = utils.getRequestArg(request, 'comment')
         mood = utils.getRequestArg(request, 'mood')
         if not mood or not comment:
             raise errors.MissingParams([_("Feedback")])
 
-        (appchange, script, args, myId) = yield self._getBasicArgs(request)
+        authInfo = request.getSession(IAuthInfo)
+        myId = authInfo.username
+        orgId = authInfo.organization
+
         tagName = 'feedback'
-        moodTagName = 'feedback-'+mood
+        moodTagName = 'feedback-' + mood
 
         feedbackDomain = config.get('Feedback', 'Domain') or 'synovel.com'
         cols = yield db.get_slice(feedbackDomain, 'domainOrgMap')
@@ -38,25 +45,34 @@ class FeedbackResource(base.BaseResource):
         synovelOrgId = cols[0].column.name
 
         tagId, tag = yield tags.ensureTag(request, tagName, synovelOrgId)
-        moodTagId, moodTag = yield tags.ensureTag(request, moodTagName, synovelOrgId)
+        moodTagId, moodTag = yield tags.ensureTag(request, moodTagName,
+                                                  synovelOrgId)
 
         # Anyone in synovel can receive feedback.
-        acl = {'accept':{'orgs':[synovelOrgId]}}
+        acl = {'accept': {'orgs': [synovelOrgId]}}
+        acl = json.dumps(acl)
+        synovelOrg = base.Entity(synovelOrgId)
+        yield synovelOrg.fetchData()
+        # createNewItem expects an entity object with has org in basic info.
+        # organizations wont have 'org' set.
+        synovelOrg.basic['org'] = synovelOrgId
 
-        item, attachments = yield utils.createNewItem(request, 'feedback',
-                                                      synovelOrgId, synovelOrgId,
-                                                      subType=mood)
+        item = yield utils.createNewItem(request, 'feedback', synovelOrg,
+                                         acl, subType=mood)
         item['meta']['org'] = synovelOrgId
         item['meta']['userId'] = myId
-        item['meta']['userOrgId'] = args['orgKey']
+        item['meta']['userOrgId'] = orgId
         item['meta']['comment'] = comment
-        item['tags'] = {tagId:synovelOrgId, moodTagId:synovelOrgId}
+        item['tags'] = {tagId: synovelOrgId, moodTagId: synovelOrgId}
 
         itemId = utils.getUniqueKey()
 
-        # XXX: Use cached values instead of calling get_count everytime
-        tagItemCount = yield db.get_count(tagId, "tagItems")
-        moodTagItemCount = yield db.get_count(moodTagId, "tagItems")
+        tagItemCount = int(tag['itemsCount'])
+        moodTagItemCount = int(moodTag['itemsCount'])
+        if tagItemCount % 10 == 7:
+            tagItemCount = yield db.get_count(tagId, "tagItems")
+        if moodTagItemCount % 10 == 7:
+            moodTagItemCount = yield db.get_count(moodTagId, "tagItems")
 
         tagItemCount += 1
         moodTagItemCount += 1
@@ -65,23 +81,23 @@ class FeedbackResource(base.BaseResource):
         yield db.batch_insert(itemId, "items", item)
         yield db.insert(tagId, "tagItems", itemId, item["meta"]["uuid"])
         yield db.insert(moodTagId, "tagItems", itemId, item["meta"]["uuid"])
-        yield db.insert(synovelOrgId, "orgTags", str(tagItemCount), "itemsCount", tagId)
-        yield db.insert(synovelOrgId, "orgTags", str(moodTagItemCount), "itemsCount", moodTagId)
+        yield db.insert(synovelOrgId, "orgTags", str(tagItemCount),
+                        "itemsCount", tagId)
+        yield db.insert(synovelOrgId, "orgTags", str(moodTagItemCount),
+                        "itemsCount", moodTagId)
 
         cols = yield db.multiget_slice([tagId, moodTagId], "tagFollowers")
         followers = utils.multiColumnsToDict(cols)
         followers = set(followers[tagId].keys() + followers[moodTagId].keys())
 
-        value = {"feed":{item['meta']['uuid']: itemId}}
+        value = {"feed": {item['meta']['uuid']: itemId}}
         muts = dict([(x, value) for x in followers])
         if muts:
             yield db.batch_mutate(muts)
 
-
     def render_GET(self, request):
         d = self.renderFeedbackForm(request)
         return self._epilogue(request, d)
-
 
     def render_POST(self, request):
         d = self.postFeedback(request)
